3338ac565a2e343dc002ad3655a76584ac54f8f4
bin55ance <betteziton@gmail.com>
2026-02-22 15:39:31 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 // db_connect.php
 
 // Database connection settings (from your FreeHost / InfinityFree control panel)
-$servername = "sql308.ezyro.com";   // SQL hostname
+$servername = "sql308.byetcluster.com";  // SQL hostname
 $username   = "ezyro_41193808";     // Database username
 $password   = "mnh7rkby";           // Database password
 $dbname     = "ezyro_41193808_teslainventory"; // Database name
