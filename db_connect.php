abc72f1fabc53c1f1ad439086a80ed6ffa18b11c
bin55ance <betteziton@gmail.com>
2026-02-22 14:57:51 +0800

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,17 @@
+<?php
+// db_connect.php
+
+// Database connection settings (from your FreeHost / InfinityFree control panel)
+$servername = "sql308.ezyro.com";   // SQL hostname
+$username   = "ezyro_41193808";     // Database username
+$password   = "mnh7rkby";           // Database password
+$dbname     = "ezyro_41193808_teslainventory"; // Database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
